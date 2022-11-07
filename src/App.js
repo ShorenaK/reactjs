3befb236a4addb16d7c1e3371a7ed62a343afe14
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-07 16:11:58 -0500

    app.css changed

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="App">
+    <div className="course-goals">
       <h1> Hi, this is Reactjs!</h1>
+      <ul className='goal-list'>
+        <li>helloo</li>
+        <li>again</li>
+        </ul>
     </div>
   );
   }
