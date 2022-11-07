64fdc16cd1abe3a282c4a46743bb5f5649be005f
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-07 15:29:08 -0500

    app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 
 import './App.css';
 import React from 'react'
+
 function App() {
-  return React.createElement('h1', {}, 'HI this is React' )
-}
+  return (
+    <div className="App">
+      <h1> Hi, this is Reactjs!</h1>
+    </div>
+  );
+  }
+  
+export default App; 
 
-export default App;
 
-
-// (
-//   <div className="App">
-    
-//   </div>
-// );
