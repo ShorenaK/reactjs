ba4a82e68c28fe9fde5f713f9ba431d8e28776a7
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-08 15:26:41 -0500

    files has been deleted and changed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 
 import './App.css';
-import React, {useState} from 'react'
-import GoalList from './components/GoalList/GoalList';
-import NewGoal from './components/NewGoal/NewGoal';
 
-function App() {
-  const [courseGoals, setcourseGoals ] = useState ([
-    {id: 'cg1', text: 'finish'},
-    {id: 'cg2', text: 'Learn all about the course' },
-    {id: 'cg3', text: 'Help other student'}
-  ]);
-
-  const addNewGoalHandler = (newGoal)=>{
-  // setcourseGoals(courseGoals.concat(newGoal))
-  setcourseGoals((prevCourseGoals)=>{
-   return prevCourseGoals.concat(newGoal)
-  })
-  }
+function App() { 
 
   return (
     <div className="course-goals">
-      <h1> Hi, this is Reactjs!</h1>
-      <NewGoal onAddGoal={addNewGoalHandler}/>
-      <GoalList goals={courseGoals}/>
+      <h1> Hi, this is New project!</h1>
+      
     </div>
   );
   }
