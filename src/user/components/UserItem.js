351c623dfd7e7ac3205b9bb26a10ba872fd1d9f4
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-09 18:42:37 -0500

    userItem done

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -3,8 +3,17 @@ import "./UserItem.css"
 
 export default function UserItem(props) {
   return (
-    <div className='user-item'>
+    <li className='user-item'>
+  <div className='user-item__content'>
+    <div className='user_item__image'>
+      <img src={props.image} alt={props.name}/>
 
     </div>
+  <div className='user-item__info'>
+    <h2>{props.name}</h2>
+   <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
+  </div>
+  </div>
+    </li>
   )
 }
