91e679f0f4c24eb2a00ebe2a1f49854671d44969
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-09 17:54:02 -0500

    files added

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./UserItem.css"
+
+export default function UserItem(props) {
+  return (
+    <div>UsserItem</div>
+  )
+}
---
+++ src/user/components/UsersList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './UserItem.css ' 
+
+
+export default function UsersList(props) {
+  return (
+    <div></div>
+  )
+}
