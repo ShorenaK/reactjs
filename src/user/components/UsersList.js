8d3d042e5d280f38d4d09d3d8bc680b5bf2a4de6
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-09 18:10:08 -0500

    userlist func to desplay user

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import UserItem from './UserItem'
 import './UserItem.css ' 
 
 
 export default function UsersList(props) {
-  return (
-    <div></div>
+    if(props.item.length === 0){ 
+    return (
+    <div className='center'>
+        <h2>NO user found</h2>
+    </div>
   )
+}
+  return  ( <ul>
+    {props.item.map(user =>{
+        return < UserItem/>
+
+    })}
+
+  </ul>
+  )
+
 }
