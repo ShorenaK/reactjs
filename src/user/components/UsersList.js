93abbdd64f8e34c78ab50cf98922a08769063c09
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-09 19:05:28 -0500

    user item done

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import UserItem from './UserItem'
-import './UserList.css ' 
+import './UsersList.css' 
 
 
 export default function UsersList(props) {
-    if(props.item.length === 0){ 
+    if(props.items.length === 0){ 
     return (
     <div className='center'>
         <h2>NO user found</h2>
@@ -12,13 +12,14 @@ export default function UsersList(props) {
   )
 }
   return  ( <ul className='user-list'>
-    {props.item.map(user =>{
+    {props.items.map(user =>{
         return < UserItem 
         key={user.id} 
         id={user.id} 
         image={user.image} 
         name={user.name}
-        placeCount={user.places}/>
+        placeCount={user.places}
+        />
 
     })}
 
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -2,7 +2,7 @@ import React from 'react'
 import UsersList from '../components/UsersList'
 
 export default function Users() {
-  const USERS = [{id: 'u1', name: 'Shorena', image: '' , place: 3}]
+  const USERS = [{id: 'u1', name: 'Shorena', image: '' , places: 3}]
   return (
     <UsersList items={USERS}/>
   )
