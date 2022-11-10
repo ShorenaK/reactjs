bf1e9cfcf037152f41129fb3068eecd3890bd90d
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-10 13:59:18 -0500

    image added

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -2,7 +2,9 @@ import React from 'react'
 import UsersList from '../components/UsersList'
 
 export default function Users() {
-  const USERS = [{id: 'u1', name: 'Shorena', image: '' , places: 3}]
+  const USERS = [{id: 'u1', name: 'Shorena',
+   image: 'https://cpmr-islands.org/wp-content/uploads/sites/4/2019/07/test.png',
+    places: 3}]
   return (
     <UsersList items={USERS}/>
   )
