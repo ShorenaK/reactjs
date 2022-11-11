e5ddf0f9658681887208d5c7bfbc73e0ec6c0fb6
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-11 18:42:08 -0500

    mainNavigation.js done

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
+import MainHeader from './MainHeader'
+import './MainNavigation.css'
 
 export default function MainNavigation() {
   return (
-    <div>MainNavigation</div>
+   <MainHeader>
+<button className='main_nagiation__menu-btn'>
+    <span></span>
+    <span></span>
+    <span></span>
+</button>
+<h1 className='main-navigation__title'>
+    <Link to="/"> Your Places </Link>
+    
+    </h1>
+    <nav>
+       ... 
+    </nav>
+   </MainHeader>
   )
 }
