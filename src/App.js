2373ec9a0f6a176c191a596e27d38a4f8af4e295
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-12 14:40:13 -0500

    css changed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom"
 import Users  from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
-
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 function App() { 
 
   return (
    <Router>
+    <MainNavigation/>
+    <main> 
     <Switch> 
     <Route path="/" exact>
   <Users />
@@ -17,6 +19,7 @@ function App() {
     </Route>
     <Redirect to="/"/>
     </Switch>
+    </main>
    </Router>
   )
   }
--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -7,10 +7,10 @@ import './MainNavigation.css'
 export default function MainNavigation() {
   return (
    <MainHeader>
-<button className='main_nagiation__menu-btn'>
-    <span></span>
-    <span></span>
-    <span></span>
+<button className='main-navigation__menu-btn'>
+   <span />
+    <span />
+    <span /> 
 </button>
 <h1 className='main-navigation__title'>
     <Link to="/"> Your Places </Link>
