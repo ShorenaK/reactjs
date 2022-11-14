55293bc45944402f6ce6200d9a78dba074c0d0e6
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-14 17:36:50 -0500

    navlink.css

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 import MainHeader from './MainHeader'
+import NavLinks from '.NavLinks'
+
 import './MainNavigation.css'
 
 export default function MainNavigation() {
@@ -17,7 +19,7 @@ export default function MainNavigation() {
     
     </h1>
     <nav>
-       ... 
+    <NavLinks/>
     </nav>
    </MainHeader>
   )
