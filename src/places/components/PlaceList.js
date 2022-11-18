155f3bdb91c0c58f10fab287bf18f2108040718a
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-17 22:17:49 -0500

    palacelist.js

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import Card from '../../shared/components/UIElements/Card';
+import PlaceItem from './PlaceItime'
 import './PlaceList.css'
 
-export default function PlaceList(prop) {
+
+
+export default function PlaceList(props) {
+  if (props.itmes.length === 0){ 
   return (
-    <div>PlaceList</div>
+    <div className='place-list center'> 
+    <Card>
+      <h2>No places found. Maybe create one ?</h2>
+      <button> Share Place</button>
+    </Card>
+    </div>
   )
+}
+return <ul className='place-list'>
+{props.itmes.map(place => <PlaceItem 
+key={place.id} 
+id={place.id} 
+image={place.imageUrl}
+title={place.title}
+description={place.description}
+address={place.address}
+creatorId={place.creator}
+coordinates={place.coordinates}
+/>
+)}
+</ul>
 }
