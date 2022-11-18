cb4c7912f0a994033fc2a9c5be1d1b0bb8ed057f
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-18 11:39:43 -0500

    Button.js created

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PlaceList from '../components/PlaceList'
+import {useParams} from 'react-router-dom'
 
 const DUMMY_PLACES =[
   {
@@ -32,5 +33,9 @@ const DUMMY_PLACES =[
 
 
 export default function UserPlaces() {
-  return <PlaceList items={DUMMY_PLACES} />
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place)=>{
+   return place.creator === userId
+  })
+  return <PlaceList items={loadedPlaces} />
 }
