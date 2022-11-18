83db06785c90819c0d804bba84410e12dd0dd000
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-18 09:19:03 -0500

    cleaned APP.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 
-import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom"
+import 
+{BrowserRouter as Router,
+ Route,
+Redirect, 
+Switch
+} from "react-router-dom"
 import Users  from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 import './App.css';
 
