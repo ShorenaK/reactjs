6aba0acb5a26ce9e6b586550eb39e0c84098f045
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-17 22:29:21 -0500

    css files complete

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -25,7 +25,7 @@ title={place.title}
 description={place.description}
 address={place.address}
 creatorId={place.creator}
-coordinates={place.coordinates}
+coordinates={place.location}
 />
 )}
 </ul>
