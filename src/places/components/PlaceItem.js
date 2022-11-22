6750bbf17b2c660cbfb60e7dd6619f38e959c32a
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-21 22:14:37 -0500

    useREf and useEffect completed

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -2,10 +2,9 @@ import React, {useState} from 'react'
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button'
 import Modal from '../../shared/components/UIElements/Modal';
-
+import Map from '../../shared/components/UIElements/Map'
 
 import './PlaceItem.css'
-
 
 export default function PlaceItem(props) {
 const [showMap, setShowMap] = useState(false)
@@ -23,7 +22,7 @@ const closeMapHandler = () => setShowMap(false)
      footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
      > 
   <div className='map-container'>
-  <h2> THE MAP ! </h2>
+      <Map center={props.coordinates} zoom={16}/> 
   </div>
 
      </Modal>
