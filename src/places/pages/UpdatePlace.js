16d0a37330e30605e6f6ca25a0736d3b92ae6272
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-25 17:58:33 -0500

    updatedplace.js done

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,54 +1,59 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 
 import {
-    VALIDATOR_REQUIRE,
-    VALIDATOR_MINLENGTH
-  } from '../../shared/util/validators';
-  
-const DUMMY_PLACES =[
-    {
-      id: 'p1', 
-      title: 'Empire State Building', 
-      description: 'One of the most famous sky scrapers in the world!', 
-      imageUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg', 
-      address: '20 W 34th St., New York, NY 10001', 
-      location: {
-        lat: 40.7484405, 
-        lng: -73.9878531
-      },
-      creator: 'u1'
-    }, 
-    {
-      id: 'p2', 
-      title: 'Empire State Building', 
-      description: 'One of the most famous sky scrapers in the world!', 
-      imageUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg', 
-      address: '20 W 34th St., New York, NY 10001 ', 
-      location: {
-        lat: 40.7484405, 
-        lng: -73.9878531
-      },
-      creator: 'u2'
-    }
-  ]
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from '../../shared/util/validators';
 
-export default function UpdatePlace() {
-    const placeId= useParams().placeId
-    const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId)
-  
-  if(!identifiedPlace){
-    return (
-        <div className="center">
-            <h2> Could not find place</h2>
-        </div>
-    )
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u2'
   }
+];
+
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+
+  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+
+  if (!identifiedPlace) {
     return (
-    <form >
- <Input
+      <div className="center">
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form>
+      <Input
         id="title"
         element="input"
         type="text"
@@ -69,7 +74,11 @@ export default function UpdatePlace() {
         value={identifiedPlace.description}
         valid={true}
       />
-
+      <Button type="submit" disabled={true}>
+        UPDATE PLACE
+      </Button>
     </form>
-  )
-}
+  );
+};
+
+export default UpdatePlace;
