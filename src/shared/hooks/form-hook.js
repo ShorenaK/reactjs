f1cd834a1bb4ad65eeec25e44b345dac55736ced
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-25 20:04:26 -0500

    hooks.js functiones

--- src/shared/hooks/form-hook.js
+++ src/shared/hooks/form-hook.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, {useCallback, useReducer}  from 'react'
 
-export default function form-hook() {
-  return (
-    <div>form-hook</div>
-  )
+
+const formReducer = (state, action)=>{
+    switch(action.type){
+      case 'INPUT_CHANGE':
+        let formIsValid = true
+        for (const inputId in state.inputs){
+          if (inputId === action.inputId){
+            formIsValid = formIsValid && action.isValid
+          }else{
+            formIsValid = formIsValid && state.inputs[inputId].isValid
+          }
+        }
+        return{
+          ...state, 
+          inputs: {
+            ...state.inputs, 
+            [action.inputId]: {value:action.value, isValid: action.isValid }
+          },
+          isValid: formIsValid
+        }
+    default:
+    return state;
+    }
+  }
+
+
+export default function useForm() {
+    const [formState, dispatch]=useReducer(formReducer, {
+        input: {
+          title: {
+            value: '', 
+            isValid: false
+          }, 
+          description: {
+            value: '', 
+            isValid: false
+          }, 
+          address:{
+            value: '', 
+            isValid: false
+          }
+        }, 
+        isValid: false
+      })
+ 
 }
