7afc91ca419d808277638fb40f7cbd3b80108fef
ShorenaK <shorenaanzhilov@gmail.com>
2022-11-26 13:46:28 -0500

    App.js has been updated

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState, useCallback} from "react";
 import 
 {BrowserRouter as Router,
  Route,
@@ -10,12 +11,21 @@ import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./user/pages/Auth";
 import MainNavigation from './shared/components/Navigation/MainNavigation';
-
+import { AuthContext } from "./shared/context/auth-context";
 import './App.css';
 
 function App() { 
-
+  const [isLoggedIn, seIsLoggedIn] = useState(false)
+  const login = useCallback(() =>{
+seIsLoggedIn(true)
+  }, [])
+  const logout = useCallback(() =>{
+seIsLoggedIn(false)
+  }, [])
+  
   return (
+    <AuthContext.Provider value={{isLoggedIn: isLoggedIn, login: login, logout:logout}}
+    > 
    <Router>
     <MainNavigation/>
     <main> 
@@ -47,6 +57,7 @@ function App() {
     </main>
 
    </Router>
+   </AuthContext.Provider>
   )
   }
   
--- src/shared/context/auth-context.js
+++ src/shared/context/auth-context.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import { createContext } from 'react'
 
+export const AuthContext = createContext({
+    isLoggedIn: false, login: () => {}, logout: ()=> {} })
 
-
-export default function () {
-  return (
-    <div>auth-context</div>
-  )
-}
