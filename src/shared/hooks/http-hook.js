de6621910ce6fc16347448673677f23db3721cba
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-20 15:46:01 -0500

    http-hook.js created

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+export const useHttpClient = () =>{
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+
+    const sendRequest = () => {
+        
+    }
+}
