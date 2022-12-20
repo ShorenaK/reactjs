17f9bef536d6c7716276cd3e33bf5ad2e210ac01
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-20 16:13:37 -0500

    http-hook

--- src/shared/hooks/http-hook.js
+++ src/shared/hooks/http-hook.js
@@ -4,11 +4,17 @@ export const useHttpClient = () =>{
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState();
 
-    const sendRequest = (url, method = 'GET', body = null, headers={}) => {
-        fetch(url, {
+    const sendRequest = async (url, method = 'GET', body = null, headers={}) => {
+        const response =  await  fetch(url, {
             method, 
             body, 
             headers
         })
+
+        const responseData = await response.json()
+
+        if(!response.ok){
+            throw new Error(responseData.message)
+           }
     }
 }
