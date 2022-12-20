d17175754141d18c32fab207de9a3016b3f388cf
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-19 20:29:58 -0500

    auth json.stringify added

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -54,9 +54,19 @@ const Auth = () => {
     setIsLoginMode(prevMode => !prevMode);
   };
 
-  const authSubmitHandler = event => {
+  const authSubmitHandler = async event => {
     event.preventDefault();
-    console.log(formState.inputs);
+  fetch('http://localhost:7000/api/users/signup', {
+    method: 'POST', 
+    headers: {
+      "Content-Type": "application/json"
+    }, 
+    body: JSON.stringify({
+      name: formState.inputs.name.value, 
+      email:formState.inputs.email.value , 
+      password: formState.inputs.password.value
+    })
+  })
     auth.login();
   };
 
