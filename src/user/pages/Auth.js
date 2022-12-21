5c14235ec6ddd9a9abf32b348a5f4a4153a041e1
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-21 18:04:21 -0500

    files updated

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -40,7 +40,7 @@ const Auth = () => {
       setFormData(
         {
           ...formState.inputs,
-          name: undefined, 
+          name: undefined,
           image: undefined
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
@@ -54,9 +54,9 @@ const Auth = () => {
             isValid: false
           },
           image: {
-            value: null, 
+            value: null,
             isValid: false
-              }
+          }
         },
         false
       );
@@ -84,17 +84,15 @@ const Auth = () => {
       } catch (err) {}
     } else {
       try {
+        const formData = new FormData();
+        formData.append('email', formState.inputs.email.value);
+        formData.append('name', formState.inputs.name.value);
+        formData.append('password', formState.inputs.password.value);
+        formData.append('image', formState.inputs.image.value);
         const responseData = await sendRequest(
           'http://localhost:7000/api/users/signup',
           'POST',
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value
-          }),
-          {
-            'Content-Type': 'application/json'
-          }
+          formData
         );
 
         auth.login(responseData.user.id);
@@ -121,7 +119,9 @@ const Auth = () => {
               onInput={inputHandler}
             />
           )}
-          {!isLoginMode && <ImageUpload center id="image" onInput={inputHandler} />}
+          {!isLoginMode && (
+            <ImageUpload center id="image" onInput={inputHandler} />
+          )}
           <Input
             element="input"
             id="email"
